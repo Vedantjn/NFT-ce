7c87383c54499f52489a29ef213ca13767904f6f
Vedant Jain <vedant.jain.indore@gmail.com>
2022-07-10 13:54:19 +0530

    first commit

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+  const MyNFT = await ethers.getContractFactory("MyNFT");
+
+  // Start deployment, returning a promise that resolves to a contract object
+  const myNFT = await MyNFT.deploy();
+  console.log("Contract deployed to address:", myNFT.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+//0xd0303736f4735Ea1529F6E94c6b59a281bB523e3
